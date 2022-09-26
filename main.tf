45c87f2fa2b95e756f0e1492c6146de5f91d642f
pxilips <58148159+pxilips@users.noreply.github.com>
2022-09-26 12:10:55 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,92 @@
+# Create resource group
+resource "azurerm_resource_group" "azure-terraform-test" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "my-VPC" {
+  name                = "my-VPC"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my-SUBNET" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.azure-terraform-test.name
+  virtual_network_name = azurerm_virtual_network.my-VPC.name
+  address_prefixes     = ["10.0.128.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_public_ip" {
+  name                = "Public_IP"
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
+  allocation_method   = "Dynamic"
+}
+
+# Create network interface
+resource "azurerm_network_interface" "my-NIC" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my-SUBNET.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "SGA" {
+  network_interface_id      = azurerm_network_interface.my-NIC.id
+  network_security_group_id = azurerm_network_security_group.my-SG.id
+}
+
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "my-Windows Server" {
+  name                = "my-Windows Server"
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
+  location            = azurerm_resource_group.azure-terraform-test.location
+  size                = "Standard_D1_v2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.my-NIC.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    type        = string
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+  }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my-SG" {
+  name                = "Allow_RDP"
+  location            = azurerm_resource_group.azure-terraform-test.location
+  resource_group_name = azurerm_resource_group.azure-terraform-test.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  default     = "westeurope"
+  description = "Location Resource Group"
+}
+
+variable "resource_group_name" {
+  default     = "azure-terraform-test"
+  description = "Resourse Group Name"
+}
