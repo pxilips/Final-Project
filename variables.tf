0fccf6a456e366cf3eba23bbc7f5de42b349b564
pxilips <58148159+pxilips@users.noreply.github.com>
2022-10-01 21:21:51 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,19 @@
-variable "resource_group_location" {
-  default     = "westeurope"
-  description = "Location Resource Group"
+variable "name_RG" {
+  default     = "Server-RG"
+  description = "Resours Group Name"
 }
 
-variable "resource_group_name" {
-  default     = "azure-terraform-test"
-  description = "Resourse Group Name"
+variable "location" {
+  default     = "West Europe"
+  description = "Region"
+}
+
+variable "VPC" {
+  default     = "Server-VPC"
+  description = "VPC Name"
+}
+
+variable "Subnet-1" {
+  default     = "Server-SUBNET"
+  description = "Subnet Name"
 }
